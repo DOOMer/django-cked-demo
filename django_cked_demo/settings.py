96aa164bb1e4c882f3404efb0c955e3eba5555b3
Hemanth kumar <hemanth7787@gmail.com>
2014-12-10 14:44:41 +0530

    Update settings.py
    Grouped and added a larger set of settings

--- django_cked_demo/settings.py
+++ django_cked_demo/settings.py
@@ -112,16 +112,24 @@ ELFINDER_OPTIONS = {
 CKEDITOR_OPTIONS = {
     'skin': 'moono',
     'toolbar': [
-            [
-            '-', 'Bold', 'Italic', 'Underline', 'Strike',
-            '-', 'BulletedList', 'NumberedList',
-            '-','Blockquote','-','JustifyLeft','JustifyCenter','JustifyRight',
-            '-', 'Image', 'Link', 'Unlink', 'Anchor', 'HorizontalRule',
-            '-', 'Source','Undo', 'Redo',
-            ],
+        { 'name': 'document', 'items': [ 'Source', '-', 'Preview', '-', 'Templates' ] }, #// Defines toolbar group with name (used to create voice label) and items in 3 subgroups.
+        { 'name': 'clipboard', 'groups': [ 'clipboard', 'undo' ], 'items': [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+        { 'name': 'editing', 'groups': [ 'find', 'selection', 'spellchecker' ], 'items': [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+        { 'name': 'insert', 'items': [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+        '/',                                                                                    #// Line break - next group will be placed in new line.
+        { 'name': 'basicstyles', 'groups': [ 'basicstyles', 'cleanup' ], 'items': [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+        { 'name': 'links', 'items': [ 'Link', 'Unlink', 'Anchor' ] },
+        { 'name': 'paragraph', 'groups': [ 'list', 'indent', 'blocks', 'align', 'bidi' ], 'items': [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+        '/',
+        { 'name': 'styles', 'items': [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+        { 'name': 'colors', 'items': [ 'TextColor', 'BGColor' ] },
+        { 'name': 'tools', 'items': [ 'Maximize', 'ShowBlocks' ] },
+        { 'name': 'others', 'items': [ '-' ] },
+        { 'name': 'about', 'items': [ 'About' ] }
         ],
     'filebrowserWindowWidth': 940,
     'filebrowserWindowHeight': 450,
     'width': 900,
     'height': 480,
+    'language': 'en',
 }
